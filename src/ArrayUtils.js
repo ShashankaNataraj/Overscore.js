4381b785d5644e599e18c6fd5a49fb2c6b8ebe07
Shashanka Nataraj <shashankan.10@gmail.com>
2017-06-28 19:55:49 +0530

    Using IIFE instead of unnecessarily creating a new class for the sake of using ES6

--- src/ArrayUtils.js
+++ src/ArrayUtils.js
@@ -1,6 +1,19 @@
-class ArrayUtils {
-    constructor() {
-        return {first: this.first, head: this.first, take: this.first};
+export default (function () {
+    /**
+     * Utility function to 
+     * 
+     * @param {any} passedArray 
+     * @param {any} n 
+     * @returns 
+     */
+    function areArgumentsValid(passedArray, n) { 
+        if (passedArray === null && n === null) { 
+            return false;
+        }
+        if (arguments.length === 0 || passedArray.length === null || passedArray.length === 0 || !(passedArray instanceof Array) || typeof n !== 'number') {
+            return false;
+        }
+        return true;
     }
     /**
      * Returns the first element of an array. Passing n will return the first n elements of the array
@@ -9,14 +22,28 @@ class ArrayUtils {
      * @param {any} n
      * @memberof ArrayUtils
      */
-    first(passedArray = [], n = 1) {
-        if (arguments.length === 0 || passedArray.length === null || passedArray.length === 0 || !(passedArray instanceof Array) || typeof n !== 'number') {
+    function first(passedArray = [], n = 1) {
+        if (!areArgumentsValid(passedArray, n)) { 
             return undefined;
         }
         let slicedArray = passedArray.slice(0, n),
-            [ first, ...remaining ] = slicedArray;
+            [first, ...remaining] = slicedArray;
         return n === 1 ? first : slicedArray;
     }
-}
-
-export default new ArrayUtils();
+    /**
+     * Returns everything but the last entry of the array. Especially useful on the arguments object. Pass n to exclude the last n elements from the result.
+     *
+     * @param {any} array
+     * @param {any} n
+     * @memberof ArrayUtils
+     */
+    function initial(passedArray = [], n = 1) {
+        if (!areArgumentsValid(passedArray, n)) { 
+            return undefined;
+        }
+        let subtractor = n > 0 ? n : 1;
+        let initialElements = passedArray.slice(0, passedArray.length - subtractor);
+        return initialElements;
+    }
+    return {first, head: first, take: first, initial};
+})();
--- test/ArrayUtils.js
+++ test/ArrayUtils.js
@@ -2,15 +2,30 @@ import ArrayUtils from '../src/ArrayUtils';
 
 const assert = require('assert'),
   scaffoldArray = [1, 2, 3, 4];
-describe('ArrayUtils', function () {
-  describe('#first()', function () {
-    it('Should return undefined when no values are passed', () => assert.equal(ArrayUtils.first(), undefined));
-    it('Should return the first element when only an array is passed', () => assert.equal(ArrayUtils.first(scaffoldArray), 1));
+describe('ArrayUtils', () => {
+  describe('#first()', () => {
+    it('Should return undefined when null values are passed', () => assert.strictEqual(ArrayUtils.first(null, null), undefined));
+    it('Should return undefined when undefined values are passed', () => assert.strictEqual(ArrayUtils.first(undefined, undefined), undefined));
+    it('Should return undefined when no values are passed', () => assert.strictEqual(ArrayUtils.first(), undefined));
+    it('Should return the first element when only an array is passed', () => assert.strictEqual(ArrayUtils.first(scaffoldArray), 1));
     it('Should return the first n elements when n is specified', () => assert.deepEqual(ArrayUtils.first(scaffoldArray, 2), [1, 2]));
-    it('Should alias the first n elements when n is specified', () => assert.deepEqual(ArrayUtils.first(scaffoldArray, 2), [1, 2]));
     it('Should be available as #head and #take', () => {
       assert.strictEqual(ArrayUtils.first.prototype, ArrayUtils.head.prototype);
       assert.strictEqual(ArrayUtils.first.prototype, ArrayUtils.take.prototype);
     });
+  });
+  describe('#initial', () => {
+    it('Should return undefined when null args are passed', () => assert.strictEqual(ArrayUtils.initial(null, null), undefined));
+    it('Should return undefined when undefined args are passed', () => assert.strictEqual(ArrayUtils.initial(undefined, undefined), undefined));
+    it('Should return undefined when no arguments are passed', () => assert.strictEqual(ArrayUtils.initial(), undefined));
+    it('Should return everything but the last entry of the array when only array is passed', () => assert.deepEqual(ArrayUtils.initial(scaffoldArray), [1, 2, 3]));
+    it('Passing an array and n excludes last n elements from the result', () => {
+      assert.deepEqual(ArrayUtils.initial(scaffoldArray, 2), [1, 2]);
+      assert.deepEqual(ArrayUtils.initial(scaffoldArray, 3), [1]);
+    });
+  });
+  describe('#last', () => {
+    it('Should return undefined when null args are passed', () => assert.strictEqual(ArrayUtils.last(null, null), undefined));
+    it('Should return undefined when undefined args are passed', () => assert.strictEqual(ArrayUtils.last(null, null), undefined));
   });
 });
